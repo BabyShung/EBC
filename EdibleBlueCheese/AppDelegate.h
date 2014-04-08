6e5db038cf355c1b8affb3a2cca68199d1f95635
BabyShung <hao-zheng@uiowa.edu>
2014-04-08 10:59:56 -0500

    Initial Commit

---
+++ EdibleBlueCheese/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EdibleBlueCheese
+//
+//  Created by Hao Zheng on 4/8/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EdibleBlueCheese/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EdibleBlueCheese
+//
+//  Created by Hao Zheng on 4/8/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
